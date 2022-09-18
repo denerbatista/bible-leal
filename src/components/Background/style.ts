27492a1d919591f782425b4f2641f51013d130a8
Dener Gomes Batista <denerbatista@icloud.com>
2022-09-18 16:08:20 -0300

    feat: better resolution of the background image

--- src/components/Background/style.ts
+++ src/components/Background/style.ts
@@ -7,24 +7,21 @@ position: absolute;
 width: 95vw;
 min-height: 90vh;
 left: 2.55vw;
-top: 38px;
-background-image: url("https://cdn.discordapp.com/attachments/985645895779508254/1021105587775344640/backgroud-books.png");
+top: 5vh;
+background-image: url("https://cdn.discordapp.com/attachments/985645895779508254/1021135100773138513/backgroud-books-2.png");
 mix-blend-mode: normal;
 border-radius: 75px;
 `;
 
 export const BackgroundBg = styled.div`
 box-sizing: border-box;
-
 position: absolute;
 width: 95vw;
 min-height: 90vh;
 left: 2.55vw;
-top: 38px;
-
+top: 5vh;
 background: #FFFFFF;
 mix-blend-mode: normal;
 opacity: 0.4;
-border: 1px solid #000000;
 border-radius: 75px;
 `
