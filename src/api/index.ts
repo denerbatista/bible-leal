492cf65960c71683a1070884d7a82ed60afcbf29
Dener Gomes Batista <100161372+denerbatista@users.noreply.github.com>
2023-09-05 13:51:41 -0300

    Update index.ts change baseUrl

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "bibli-leal-server-production.up.railway.app",
+  baseURL: "http://bibli-leal-server-production.up.railway.app",
 });
